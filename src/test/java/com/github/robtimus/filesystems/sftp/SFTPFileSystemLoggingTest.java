298e15957f8d885584240cfcabd3f25b11a53861
Rob Spoor <rob@icemanx.nl>
2022-12-10 15:17:45 +0100

    Use TestLogger JUnit extension

--- src/test/java/com/github/robtimus/filesystems/sftp/SFTPFileSystemLoggingTest.java
+++ src/test/java/com/github/robtimus/filesystems/sftp/SFTPFileSystemLoggingTest.java
@@ -25,88 +25,35 @@ import static org.hamcrest.Matchers.startsWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystems;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.apache.log4j.Appender;
 import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
-import org.apache.log4j.varia.NullAppender;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import com.github.robtimus.junit.support.extension.testlogger.Reload4jLoggerContext;
+import com.github.robtimus.junit.support.extension.testlogger.TestLogger;
 
 @SuppressWarnings("nls")
 class SFTPFileSystemLoggingTest extends AbstractSFTPFileSystemTest {
 
-    private static Logger logger;
-    private static Level originalLevel;
-    private static List<Appender> originalAppenders;
-    private static List<Appender> originalRootAppenders;
-
-    private Appender appender;
-
-    @BeforeAll
-    static void setupLogging() {
-        logger = LogManager.getLogger(SSHChannelPool.class.getPackage().getName());
-        originalLevel = logger.getLevel();
-        logger.setLevel(Level.TRACE);
-        originalAppenders = getAllAppenders(logger);
-        logger.removeAllAppenders();
-
-        Logger root = LogManager.getRootLogger();
-        originalRootAppenders = getAllAppenders(root);
-        root.removeAllAppenders();
-    }
-
-    private static List<Appender> getAllAppenders(Logger l) {
-        List<Appender> appenders = new ArrayList<>();
-        for (@SuppressWarnings("unchecked") Enumeration<Appender> e = l.getAllAppenders(); e.hasMoreElements(); ) {
-            appenders.add(e.nextElement());
-        }
-        return appenders;
-    }
-
-    @AfterAll
-    static void clearLogging() {
-        logger.setLevel(originalLevel);
-        for (Appender appender : originalAppenders) {
-            logger.addAppender(appender);
-        }
-
-        Logger root = LogManager.getRootLogger();
-        for (Appender appender : originalRootAppenders) {
-            root.addAppender(appender);
-        }
-    }
-
-    @BeforeEach
-    void setupAppender() {
-        appender = spy(new NullAppender());
-        logger.addAppender(appender);
-    }
-
-    @AfterEach
-    void clearAppender() {
-        logger.removeAppender(appender);
-    }
-
     @Test
-    void testLogging() throws IOException {
+    void testLogging(@TestLogger.ForClass(SSHChannelPool.class) Reload4jLoggerContext logger) throws IOException {
+        Appender appender = mock(Appender.class);
+        logger.setLevel(Level.TRACE)
+                .addAppender(appender)
+                .useParentAppenders(false);
+
         URI uri = getURI();
         try (SFTPFileSystem fs = newFileSystem(uri, createEnv())) {
             SFTPFileSystemProvider.keepAlive(fs);
